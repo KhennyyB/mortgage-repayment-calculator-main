81b91b993010d7566a5c09678bc493eab1da423c
Martinsgfx <martos.stifler@gmail.com>
2025-02-21 13:12:41 +0100

    second commit

--- index-me.js
+++ index-me.js
@@ -14,36 +14,36 @@ function calculateMortgage() {
 
   if (isRepayment) {
     totalAmount =
-      amount * monthlyRate * Math.pow(1 + monthlyRate, totalPayment) /
+      (amount * monthlyRate * Math.pow(1 + monthlyRate, totalPayment)) /
       (Math.pow(1 + monthlyRate, totalPayment) - 1);
     totalRepayment = totalAmount * totalPayment;
   } else {
     totalAmount = amount * monthlyRate;
-    totalRepayment = (totalAmount * totalPayment) + amount;
+    totalRepayment = totalAmount * totalPayment + amount;
   }
 
-  document.getElementById('total-amount').textContent = totalAmount.toFixed(2)
-  .replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-document.getElementById('balance').textContent = totalRepayment.toFixed(2)
-  .replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-
-  
+  document.getElementById("total-amount").textContent =
+    `£ ${totalAmount.toFixed(2)}`.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  document.getElementById("balance").textContent = `£ ${totalRepayment.toFixed(
+    2
+  )}`.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
 function clearForm() {
-    document.getElementById('mortgage-amount').value = '';
-    document.getElementById('mortgage-term').value = '';
-    document.getElementById('interest-rate').value = '';
-    document.getElementById('repayment').checked = true;
-    document.getElementById('total-amount').textContent = '0.00';
-    document.getElementById('balance').textContent = '0.00';
+  document.getElementById("mortgage-amount").value = "";
+  document.getElementById("mortgage-term").value = "";
+  document.getElementById("interest-rate").value = "";
+  document.getElementById("repayment").checked = true;
+  document.getElementById("total-amount").textContent = "0.00";
+  document.getElementById("balance").textContent = "0.00";
 }
 
-
-document.getElementById('mortgage-amount').addEventListener('input', function(e) {
-    let value = e.target.value.replace(/,/g, '');
+document
+  .getElementById("mortgage-amount")
+  .addEventListener("input", function (e) {
+    let value = e.target.value.replace(/,/g, "");
     if (value) {
-      value = parseInt(value).toLocaleString('en-GB');
+      value = parseInt(value).toLocaleString("en-GB");
       e.target.value = value;
     }
-  }); 
+  });
